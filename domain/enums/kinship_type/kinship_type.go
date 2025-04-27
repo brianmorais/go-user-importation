826c8d67e124de1251bedd0a72fc298a771d73dc
Brian Leonardo de Morais <b.leonardo.morais@gmail.com>
2025-04-27 09:24:55 -0300

    Remoção de comentários desnecessários

--- domain/enums/kinship_type/kinship_type.go
+++ domain/enums/kinship_type/kinship_type.go
@@ -1,18 +1,9 @@
 package kinship_type
 
 const (
-	// Pai/Mãe
-	Parent int32 = 0
-
-	// Cônjuge
-	Spouse int32 = 1
-
-	// Filho(a)
-	Child int32 = 2
-
-	// Padastro
+	Parent     int32 = 0
+	Spouse     int32 = 1
+	Child      int32 = 2
 	StepParent int32 = 3
-
-	// Outros
-	Other int32 = 4
+	Other      int32 = 4
 )
